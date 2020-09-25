b36f9f296b5eafbd650cc5ba3ce3250ae7abf084
htl-leonding <t.stuetz@htl-leonding.ac.at>
2020-09-25 19:12:05 +0200

    add car-rental project

--- my-first-project/src/main/java/at/htl/ExampleResource.java
+++ my-first-project/src/main/java/at/htl/ExampleResource.java
@@ -1,16 +1,18 @@
 package at.htl;
 
+import at.htl.entity.Person;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hellokitty")
+@Path("/person")
 public class ExampleResource {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello 4ahitm!";
+    @Produces(MediaType.APPLICATION_JSON)
+    public Person hello() {
+        return new Person("dorfelmännchen");
     }
 }
---
+++ my-first-project/src/main/java/at/htl/entity/Person.java
@@ -0,0 +1,28 @@
+package at.htl.entity;
+
+public class Person {
+    
+    private String name;
+
+    public Person() {
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
