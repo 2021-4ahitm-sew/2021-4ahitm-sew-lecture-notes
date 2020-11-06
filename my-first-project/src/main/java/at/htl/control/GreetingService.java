825076a58b482fcc8b8675c550c765e7af493063
htl-leonding <t.stuetz@htl-leonding.ac.at>
2020-11-06 09:39:17 +0100

    add traditional logging

--- my-first-project/src/main/java/at/htl/control/GreetingService.java
+++ my-first-project/src/main/java/at/htl/control/GreetingService.java
@@ -1,13 +1,22 @@
 package at.htl.control;
 
+import org.jboss.logging.Logger;
+
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Dependent;
+import javax.enterprise.context.RequestScoped;
 
 @ApplicationScoped
+//@RequestScoped
 public class GreetingService {
+    
+    private static final Logger logger = Logger
+            .getLogger(GreetingService.class.getSimpleName());
     
     int counter;
 
     public String greeting(String name) {
+        logger.info(String.format("Hello %s (%d x verwendet)", name, ++counter));
         return String.format("Hello %s (%d x verwendet)", name, ++counter);
     }
     
